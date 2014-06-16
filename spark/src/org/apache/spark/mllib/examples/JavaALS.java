9c851c660dd13e64c18e64f27ce34dc4bbb0acbb
duanfa <duanfa@xinnet.com>
2014-06-16 17:26:27 +0800

    aaaaaaaaaaaaa

--- spark/src/org/apache/spark/mllib/examples/JavaALS.java
+++ spark/src/org/apache/spark/mllib/examples/JavaALS.java
@@ -35,56 +35,52 @@ import scala.Tuple2;
  */
 public final class JavaALS {
 
-  static class ParseRating extends Function<String, Rating> {
-    private static final Pattern COMMA = Pattern.compile(",");
+	static class ParseRating extends Function<String, Rating> {
+		private static final Pattern COMMA = Pattern.compile(",");
 
-    @Override
-    public Rating call(String line) {
-      String[] tok = COMMA.split(line);
-      int x = Integer.parseInt(tok[0]);
-      int y = Integer.parseInt(tok[1]);
-      double rating = Double.parseDouble(tok[2]);
-      return new Rating(x, y, rating);
-    }
-  }
+		@Override
+		public Rating call(String line) {
+			String[] tok = COMMA.split(line);
+			int x = Integer.parseInt(tok[0]);
+			int y = Integer.parseInt(tok[1]);
+			double rating = Double.parseDouble(tok[2]);
+			return new Rating(x, y, rating);
+		}
+	}
 
-  static class FeaturesToString extends Function<Tuple2<Object, double[]>, String> {
-    @Override
-    public String call(Tuple2<Object, double[]> element) {
-      return element._1() + "," + Arrays.toString(element._2());
-    }
-  }
+	static class FeaturesToString extends Function<Tuple2<Object, double[]>, String> {
+		@Override
+		public String call(Tuple2<Object, double[]> element) {
+			return element._1() + "," + Arrays.toString(element._2());
+		}
+	}
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    if (args.length != 5 && args.length != 6) {
-      System.err.println(
-          "Usage: JavaALS <master> <ratings_file> <rank> <iterations> <output_dir> [<blocks>]");
-      System.exit(1);
-    }
+		if (args.length != 5 && args.length != 6) {
+			System.err.println("Usage: JavaALS <master> <ratings_file> <rank> <iterations> <output_dir> [<blocks>]");
+			System.exit(1);
+		}
 
-    int rank = Integer.parseInt(args[2]);
-    int iterations = Integer.parseInt(args[3]);
-    String outputDir = args[4];
-    int blocks = -1;
-    if (args.length == 6) {
-      blocks = Integer.parseInt(args[5]);
-    }
+		int rank = Integer.parseInt(args[2]);
+		int iterations = Integer.parseInt(args[3]);
+		String outputDir = args[4];
+		int blocks = -1;
+		if (args.length == 6) {
+			blocks = Integer.parseInt(args[5]);
+		}
 
-    JavaSparkContext sc = new JavaSparkContext(args[0], "JavaALS",
-        System.getenv("SPARK_HOME"), JavaSparkContext.jarOfClass(JavaALS.class));
-    JavaRDD<String> lines = sc.textFile(args[1]);
+		JavaSparkContext sc = new JavaSparkContext(args[0], "JavaALS", System.getenv("SPARK_HOME"), JavaSparkContext.jarOfClass(JavaALS.class));
+		JavaRDD<String> lines = sc.textFile(args[1]);
 
-    JavaRDD<Rating> ratings = lines.map(new ParseRating());
+		JavaRDD<Rating> ratings = lines.map(new ParseRating());
 
-    MatrixFactorizationModel model = ALS.train(ratings.rdd(), rank, iterations, 0.01, blocks);
+		MatrixFactorizationModel model = ALS.train(ratings.rdd(), rank, iterations, 0.01, blocks);
 
-    model.userFeatures().toJavaRDD().map(new FeaturesToString()).saveAsTextFile(
-        outputDir + "/userFeatures");
-    model.productFeatures().toJavaRDD().map(new FeaturesToString()).saveAsTextFile(
-        outputDir + "/productFeatures");
-    System.out.println("Final user/product features written to " + outputDir);
+		model.userFeatures().toJavaRDD().map(new FeaturesToString()).saveAsTextFile(outputDir + "/userFeatures");
+		model.productFeatures().toJavaRDD().map(new FeaturesToString()).saveAsTextFile(outputDir + "/productFeatures");
+		System.out.println("Final user/product features written to " + outputDir);
 
-    System.exit(0);
-  }
+		System.exit(0);
+	}
 }
